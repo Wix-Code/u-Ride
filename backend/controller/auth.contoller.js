1e79911b723cf4079159af08d2f10067d3f8bfdd
Wix-Code <ogbonna428alex@gmail.com>
2025-03-06 14:22:18 +0100

    push

--- backend/controller/auth.contoller.js
+++ backend/controller/auth.contoller.js
@@ -1,8 +1,22 @@
+import { Prisma } from "@prisma/client";
+import prisma from "../utils/prisma";
+
 export const register = async () => {
+  const { email, name, password } = req.body;
   try {
-    
+    const user = await prisma.user.create(
+      {
+        data: {
+          email,
+          name,
+          password,
+        },
+      },
+    )
+    res.status(201).json({ success: true, message: "User successfully registered", user });
   } catch (error) {
     console.log(error)
+    res.status(400).json({ success: false, message: "Failed to register user" });
   }
 }
 
