89e5f67874cb012b0a4e2781b3300b8b44cf18f7
Wix-Code <ogbonna428alex@gmail.com>
2025-03-06 14:27:02 +0100

    push

--- backend/controller/auth.contoller.js
+++ backend/controller/auth.contoller.js
@@ -3,13 +3,37 @@ import prisma from "../utils/prisma";
 
 export const register = async () => {
   const { email, name, password } = req.body;
+
+  if (!email ||!name ||!password) {
+    return res.status(400).json({ success: false, message: "Please provide all required fields" });
+  }
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (existingUser) {
+    return res.status(400).json({ success: false, message: "Email already in use" });
+  }
+
+  if(!validator.isEmail(email)) {
+    return res.status(400).json({ success: false, message: "Invalid email format" });
+  }
+
+  if (password < 8) {
+    return res.status(400).json({ success: false, message: "Password must be at least 8 characters long" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  
   try {
     const user = await prisma.user.create(
       {
         data: {
           email,
           name,
-          password,
+          password:hashedPassword,
         },
       },
     )
