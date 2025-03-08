e6ac928d50e9ceef6ab55ac7beacadea3acfb4d8
Wix-Code <ogbonna428alex@gmail.com>
2025-03-08 19:50:17 +0100

    push

--- backend/route/cars.route.js
+++ backend/route/cars.route.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { verifyToken } from "../utils/verifyToken.js"
-import { getCar, getCars, postCars } from "../controller/cars.controller.js"
+import { deleteCar, getCar, getCars, postCars, updateCar } from "../controller/cars.controller.js"
 
 const router = express.Router()
 
 router.post("/", verifyToken, postCars)
 router.get("/", getCars)
 router.get("/:id", getCar)
+router.put("/:id", updateCar)
+router.delete("/:id", deleteCar)
 
 export default router
