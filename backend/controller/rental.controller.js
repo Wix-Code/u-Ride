0d6a3e2d0ffd6e318c92e9dffbf18bee35801957
Wix-Code <ogbonna428alex@gmail.com>
2025-03-08 01:14:16 +0100

    post

--- backend/controller/rental.controller.js
+++ backend/controller/rental.controller.js
@@ -2,13 +2,18 @@ import prisma from "../utils/prisma.js"
 
 export const rental = async (req, res) => {
   const userId = req.user
-  const { name, email, ...others } = req.body;
+  const { name, email, startDate, endDate, ...others } = req.body;
   if (!userId) {
     return res.status(404).json({success: true, message: "Not authorised"})
   }
+  const formattedDate = new Date(startDate).toISOString();
+  const formattedEndDate = new Date(endDate).toISOString();
   try {
     const rent = await prisma.rent.create({
       data: {
+        userId,
+        startDate : formattedDate,
+        endDate : formattedEndDate,
         name,
         email,
         ...others,
