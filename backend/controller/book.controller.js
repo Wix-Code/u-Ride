9807f124ada284aacfdbf5122123faf06394aed2
Wix-Code <ogbonna428alex@gmail.com>
2025-03-08 02:58:33 +0100

    push

--- backend/controller/book.controller.js
+++ backend/controller/book.controller.js
@@ -1,5 +1,23 @@
 export const bookRide = async () => {
+  const userId = req.user;
+  const { pickupLocation, dropoffLocation, date, time, amount, ...others } = req.body;
+  if (!userId) {
+    return res.status(404).json({ success: true, message: "Not authorised" });
+  }
   try {
+    const kl = 2000;
+    
+    const book = await prisma.ride.create({
+      data: {
+        userId,
+        pickupLocation,
+        dropoffLocation,
+        date,
+        time,
+       ...others,
+      },
+    });
+    res.status(201).json({ success: true, message: "Ride booked successfully", book });
     
   } catch (error) {
     console.log(error)
