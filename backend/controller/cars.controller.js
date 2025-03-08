b42a46e51601a215b79168d64287718015f7558c
Wix-Code <ogbonna428alex@gmail.com>
2025-03-08 18:57:03 +0100

    push

--- backend/controller/cars.controller.js
+++ backend/controller/cars.controller.js
@@ -1,6 +1,25 @@
 export const postCars = async (req, res) => {
+  const userId = req.user;
+  const { model, year, capacity, addHor, fullDay, halfDay, luggage, description, image } = req.body;
+  if (!userId) {
+    return res.status(404).json({ success: true, message: "Not authorised" });
+  }
   try {
-    
+    const car = await prisma.car.create({
+      data: {
+        userId,
+        model,
+        year,
+        capacity,
+        addHor,
+        fullDay,
+        halfDay,
+        luggage,
+        description,
+        image,
+      },
+    });
+    res.status(201).json({ success: true, message: "Car created successfully", car });
   } catch (error) {
     console.log(error)
     res.status(500).json({ success: false, message: "Failed to create car" });
