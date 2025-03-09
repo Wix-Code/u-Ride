fe295c2769c82e2f530698d9d89211d7ed9e1dcd
Wix-Code <ogbonna428alex@gmail.com>
2025-03-09 03:25:48 +0100

    push

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ import { FaLocationDot } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div className='mt-32 font-display'>
-      <div className='flex justify-between lg:flex-row max-w-[1000px] sm:flex-col sm:gap-3 lg:gap-5 lg:m-auto sm:mx-6'>
+      <div className='flex justify-between lg:flex-row max-w-[1000px] sm:flex-col sm:gap-3 lg:gap-5 lg:m-auto sm:mx-6 sm:w-full'>
         <div className='flex-1 flex flex-col gap-4'>
           <h1 className='text-[22px] text-[#1d274e] font-extrabold'>COMPANY</h1>
           <div className='uppercase text-[14px] flex flex-col gap-2'>
@@ -28,7 +28,7 @@ const Footer = () => {
           <a className='text-[16px] text-[#343a40] flex items-center gap-1' href="mailto:ogbonna428alex@gmail.com"><IoMdMail /> ogbonna428alex@gmail.com</a>
           <a className='text-[16px] text-[#343a40] flex items-center gap-1' href="tel:+2348126829146"><FaPhone /> 08126829146</a>
           <a className='text-[16px] text-[#28a745] flex items-center gap-1' href="http://"><FaLocationDot /> No 16 Banjul Street, Wuse II, Abuja.</a>
-          <img className='mt-3' src="https://s11.flagcounter.com/count2/O9J1/bg_FFFFFF/txt_000000/border_FFFFFF/columns_4/maxflags_40/viewers_0/labels_0/pageviews_1/flags_0/percent_0/" alt="" />
+          <img className='mt-3 w-full' src="https://s11.flagcounter.com/count2/O9J1/bg_FFFFFF/txt_000000/border_FFFFFF/columns_4/maxflags_40/viewers_0/labels_0/pageviews_1/flags_0/percent_0/" alt="" />
         </div>
         <div className='flex-1 flex flex-col gap-4'>
           <h1 className='text-[22px] text-[#1d274e] font-extrabold'>DOWNLOAD</h1>
@@ -40,7 +40,7 @@ const Footer = () => {
         </div>
       </div>
       <div className='bg-[#28a745] py-10 text-center mt-20'>
-        <p className='text-[14px] text-white'>© 2021-2025 NARAXI. All rights reserved</p>
+        <p className='lg:text-[14px] sm:text-[12px] text-white'>© 2021-2025 NARAXI. All rights reserved</p>
       </div>
     </div>
   )
