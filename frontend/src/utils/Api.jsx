29a62f55c83a77985abc0163d8a27bba2314b64f
Wix-Code <ogbonna428alex@gmail.com>
2025-03-17 10:12:45 +0100

    push

--- frontend/src/utils/Api.jsx
+++ frontend/src/utils/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://u-ride.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
