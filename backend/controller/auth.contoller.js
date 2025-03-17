8f9502c06fa6c4c34e1b7c9c997256ce25ba93ad
Wix-Code <ogbonna428alex@gmail.com>
2025-03-17 19:32:02 +0100

    push

--- backend/controller/auth.contoller.js
+++ backend/controller/auth.contoller.js
@@ -69,7 +69,11 @@ export const login = async (req, res) => {
     const token = jwt.sign({ id: user.id }, process.env.JWT_TOKEN, { expiresIn: "1hr" })
     
 
-    return res.cookie("accessToken", token).status(201).json({ success: true, message: "Login successful", others, token });
+    return res.cookie("accessToken", token,{
+      httpOnly: true,  
+      secure: true,      
+      sameSite: "None", 
+    }).status(201).json({ success: true, message: "Login successful", others, token });
   } catch (error) {
     console.log(error)
     res.status(400).json({ success: false, message: "Failed to login user" });
