41614a007a1c3444e8fad4c1bb08ce6f491ad174
Wix-Code <ogbonna428alex@gmail.com>
2025-04-11 11:17:10 +0100

    push

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
           </div>
         </div>
         <div data-aos="fade-left" className='flex-1 sm:w-[100%]'>
-          <img className='flex-1 w-[100] sm:w-[100%]' src="https://mlszn6rjkywy.i.optimole.com/w:850/h:803/q:mauto/f:best/https://nairaxi.ng/wp-content/uploads/2023/06/Nairaxi-the-hybrid-application-for-movement-shopping-and-parcel-delivery-1-min.png" alt="" />
+          <img className='flex-1 w-[100] h-[100%] sm:w-[100%]' src="https://mlszn6rjkywy.i.optimole.com/w:850/h:803/q:mauto/f:best/https://nairaxi.ng/wp-content/uploads/2023/06/Nairaxi-the-hybrid-application-for-movement-shopping-and-parcel-delivery-1-min.png" alt="" />
         </div>
       </div>
       <div className='max-w-[1100px] m-auto lg:flex lg:flex-row items-center justify-between gap-6 sm:flex sm:flex-col sm:gap-4'>
