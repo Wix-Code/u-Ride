c14100103e55a5eb0c86b7be7a1df5e142d1ecb0
Wix-Code <ogbonna428alex@gmail.com>
2025-04-29 00:06:09 +0100

    push

--- frontend/src/utils/Loader.jsx
+++ frontend/src/utils/Loader.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Loader = () => {
+const Loader = ({formRef}) => {
   return (
-    <div className='flex justify-center items-center h-[100%]'>
+    <div ref={formRef} className='flex justify-center items-center h-[100%]'>
       <img className='sm:w-[150px] lg:w-[250px]' src='https://mlszn6rjkywy.i.optimole.com/w:auto/h:auto/q:mauto/f:best/https://nairaxi.ng/wp-content/uploads/2024/04/Nairaxi-Luxury-Car-Hire_Rentals-Ride-Hailing-Transport-Technology.png' style={{ zIndex: 88, animation: "blinker 1s linear infinite"}} />
     </div>
   )
